99c56c61496cbeba4d860a289c145a51e1b63a6e
savchenko alexander <ext.adsavchenko@tinkoff.ru>
2022-01-15 14:50:22 +0400

    refactor common folder

--- schemas.ts
+++ schemas.ts
@@ -1,23 +1,20 @@
-import { idSchemaStructure, throwError } from 'src/common/validators'
+import { assertMatchPattern, assertNotUndefined, assertString } from '@savchenko91/schema-validator/dist/assertions'
 import { only } from '@savchenko91/schema-validator/dist/structure-validators'
 import { validate } from '@savchenko91/schema-validator/dist/validate'
-import { assertMatchPattern, assertNotUndefined, assertString } from '@savchenko91/schema-validator/dist/assertions'
 
-export const createUserSchema = {
+import { idSchemaStructure } from './validators'
+
+export const createUserSchema = only({
   username: validate([assertNotUndefined, [assertMatchPattern, /^(\w*)$/]]),
   password: validate([assertNotUndefined, assertString]),
   email: validate([assertNotUndefined, assertString]),
   name: validate([assertString]),
-}
+})
 
-export const validateCreateUser = throwError('createUserSchema')(only(createUserSchema))
-
-export const updateUserSchema = {
+export const updateUserSchema = only({
   ...idSchemaStructure,
   username: validate([[assertMatchPattern, /^(\w*)$/]]),
   password: validate([assertString]),
   email: validate([assertString]),
   name: validate([assertString]),
-}
-
-export const validateUpdateUser = throwError('updateUserSchema')(only(updateUserSchema))
+})
