1763b69fa4696e68157d0da26c97ad16d0cea137
Savchenko Alexander <sashulinator@gmail.com>
2022-04-20 13:19:59 +0400

    add rules for password field

--- schemas.ts
+++ schemas.ts
@@ -11,18 +11,28 @@ import {
   wrap,
 } from '@savchenko91/schema-validator'
 
+import { CreateUser, UpdateUser } from './types'
+
 const bindedWrap = wrap.bind({ handleError: buildErrorTree })
 
 export const createUserValidator = bindedWrap(
-  only({
+  only<CreateUser>({
     username: and(string, withValue(/^(\w*)$/, matchPattern)),
-    password: and(string, notEmptyString),
+    password: and(
+      string,
+      notEmptyString,
+      withValue(/[A-Z]/, matchPattern),
+      withValue(/[a-z]/, matchPattern),
+      withValue(/[0-9]/, matchPattern)
+    ),
     email: and(string, withValue(/@.*\.*./, matchPattern)),
     fullname: or(string, _undefined),
   })
 )
 
-export const updateUserValidator = only({
-  id: string,
-  ...createUserValidator,
-})
+export const updateUserValidator = bindedWrap(
+  only<UpdateUser>({
+    id: string,
+    ...createUserValidator,
+  })
+)
